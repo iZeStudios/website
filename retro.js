bfc244fbb65053bdb8014e84629b2d7f9a7a4635
pup64hcp <pup64hcp@gmail.com>
2024-02-29 22:14:25 -0500

    added dark mode

--- retro.js
+++ retro.js
@@ -1,3 +1,39 @@
+const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+if (prefersDarkTheme.matches) {
+    document.body.classList.add("dark");
+    localStorage.setItem('prefersDarkTheme', 'true');
+}
+
+if (localStorage.getItem('prefersRetro') === null) {
+    localStorage.setItem('prefersRetro', 'true');
+}
+
+if (localStorage.getItem('prefersRetro') === 'true') {
+    document.body.classList.add('retro');
+} else {
+    document.body.classList.remove('retro');
+}
+
+if (localStorage.getItem('prefersDarkTheme') === 'true') {
+    document.body.classList.add("dark");
+} else {
+    document.body.classList.remove("dark");
+}
+
 document.getElementById("retro").addEventListener("click", () => {
     document.body.classList.toggle("retro");
+    if (localStorage.getItem('prefersRetro') === 'true') {
+        localStorage.setItem('prefersRetro', 'false');
+    } else {
+        localStorage.setItem('prefersRetro', 'true');
+    }
+});
+
+document.getElementById("dark").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (localStorage.getItem('prefersDarkTheme') === 'true') {
+        localStorage.setItem('prefersDarkTheme', 'false');
+    } else {
+        localStorage.setItem('prefersDarkTheme', 'true');
+    }
 });
